71bf67a1fad221a4f1644c6f50a5eaacc5bcd7e3
Kat Crews <katfreitas808@gmail.com>
2022-06-19 15:08:20 -0700

    results window added

--- scripts.js
+++ scripts.js
@@ -1,101 +1,100 @@
 const startBtn = document.getElementById('start_btn');
 const nextBtn = document.getElementById('next_btn');
 const questionCont = document.getElementById('question-cont');
-const answer = document.getElementsByClassName('btn');
-startBtn.addEventListener('click', playGame);
+const answer = document.getElementById('choose');
+const btn = document.getElementsByClassName('btn');
+const resultsCont = document.getElementById('final_result');
 
-let shuffle, QuestionsIndex;
-let questionCounter = 0;
+startBtn.addEventListener('click', playGame);
 
 const Questions = [{
     id: 0,
     q: 'What fruit is indigenous to Hawaii?',
     a: [
-        { text: "hala kahiki or the pineapple", isCorrect: false },
-        { text: "hala or the screwpine", isCorrect: true },
-        { text: "lilikoi or the passionfruit", isCorrect: false },
-        { text: "manako or the mango", isCorrect: false },
+        { text: "hala kahiki or the pineapple" },
+        { text: "hala or the screwpine", isCorrect: 1},
+        { text: "lilikoi or the passionfruit" },
+        { text: "manako or the mango" }
     ]
 },
 {
     id: 1,
     q: 'Traditional hula dances:',
     a: [
-        { text: "tells stories using hands and body movements", isCorrect: true },
-        { text: "uses hip movements to do story telling", isCorrect: false },
-        { text: "involves slapping the chest and thighs, usually accompanied by a haka", isCorrect: false },
-        { text: "was invented in 1802", isCorrect: false },
+        { text: "tells stories using hands and body movements", isCorrect: 1},
+        { text: "uses hip movements to do story telling",  },
+        { text: "involves slapping the chest and thighs, usually accompanied by a haka",  },
+        { text: "was invented in 1802",  }
     ]
-    
+
 },
 {
     id: 2,
     q: "The 'ukulele was invented by the:",
     a: [
-        { text: "Hawaiians", isCorrect: false },
-        { text: "Filipinos", isCorrect: false },
-        { text: "Portuguese", isCorrect: true },
-        { text: "British", isCorrect: false },
+        { text: "Hawaiians"},
+        { text: "Filipinos" },
+        { text: "Portuguese", isCorrect: 1},
+        { text: "British" }
     ]
-    
+
 },
 {
     id: 3,
     q: 'How many islands are located in Hawaii?',
     a: [
-        { text: "132", isCorrect: true },
-        { text: "8", isCorrect: false },
-        { text: "4", isCorrect: false },
-        { text: "6", isCorrect: false },
+        { text: "132", isCorrect: 1},
+        { text: "8" },
+        { text: "4"},
+        { text: "6" }
     ]
-    
+
 },
 {
     id: 4,
     q: 'Mahalo means:',
     a: [
-        { text: "hello", isCorrect: false },
-        { text: "goodbye", isCorrect: false },
-        { text: "trash", isCorrect: false },
-        { text: "thank you", isCorrect: true },
+        { text: "hello"},
+        { text: "goodbye"},
+        { text: "trash"},
+        { text: "thank you", isCorrect: 1}
     ]
-    
+
 },
 {
     id: 5,
     q: 'Pele, the goddess of fire and volcanoes has been known to manifest around the islands as:',
     a: [
-        { text: "a white dog", isCorrect: false },
-        { text: "an old woman", isCorrect: true },
-        { text: "a wild boar", isCorrect: false },
-        { text: "a red bird", isCorrect: false },
+        { text: "a white dog"},
+        { text: "an old woman", isCorrect: 1},
+        { text: "a wild boar"},
+        { text: "a red bird"}
     ]
-    
 },
 ]
-var start = true;
+var score = 0;
 
 function playGame() {
     startBtn.classList.add('hide');
     nextBtn.classList.remove('hide');
     questionCont.classList.remove('hide');
-    shuffle = Questions.sort(()=> Math.random() * Questions.length);
-    QuestionsIndex = 0;
-    score = 0;
-    questionCounter = 0;
+
+    // shuffle = Questions.sort(() => Math.random() * Questions.length);
+    // score = 0;
+    iterate();
 };
 
-function iterate(id) {
-    
+function iterate() {
+
     const question = document.getElementById("question");
-    
+
     question.innerText = Questions[id].q;
+    nextBtn.classList.add('hide');
 
     const op1 = document.getElementById('op1');
     const op2 = document.getElementById('op2');
     const op3 = document.getElementById('op3');
     const op4 = document.getElementById('op4');
-
 
     op1.innerText = Questions[id].a[0].text;
     op2.innerText = Questions[id].a[1].text;
@@ -111,59 +110,66 @@ function iterate(id) {
 
     op1.addEventListener("click", () => {
         selected = op1.value;
+        nextBtn.classList.remove('hide');
     })
 
-    // Show selection for op2
     op2.addEventListener("click", () => {
         selected = op2.value;
+        nextBtn.classList.remove('hide');
     })
 
-    // Show selection for op3
     op3.addEventListener("click", () => {
         selected = op3.value;
+        nextBtn.classList.remove('hide');
     })
 
-    // Show selection for op4
     op4.addEventListener("click", () => {
         selected = op4.value;
+        nextBtn.classList.remove('hide');
     })
 
-
-    // const evaluate = document.getElementsByClassName("evaluate");
-
-   // Evaluate method
-    // evaluate[0].addEventListener("click", () => {
-    //     if (selected == "true") {
-    //         result[0].innerHTML = "True";
-    //         result[0].style.color = "green";
-    //     } else {
-    //         result[0].innerHTML = "False";
-    //         result[0].style.color = "red";
-    //     }
-    // })
-}
-
-if (start) {
-    iterate("0");    
+    answer.addEventListener('click', () => {
+        if (selected == 1) {
+            // score;
+            answer.style.backgroundColor = "green";
+            console.log('correct');
+        } else {
+            answer.style.backgroundColor = "red";
+            // console.log('wrong');
+        }
+    })
 }
 
 
 var id = 0;
-  
 nextBtn.addEventListener("click", () => {
-    start = false;
-    questionCounter ++;
+    answer.style.backgroundColor = "inherit";
+    // questionCounter ++;
     if (id < 5) {
         id++;
-        iterate(id);
-    }
-}) 
-
-answer[0].addEventListener("click", () => {
-    if (selected =="true"){
-        answer[0].classList.add('correct');
+        iterate();
     } else {
-        answer[0].classList.add('wrong');
+        return result();
     }
-});
+})
+
+function result() {
+    startBtn.classList.add('hide');
+    nextBtn.classList.add('hide');
+    questionCont.classList.add('hide');
+    resultsCont.classList.remove('hide');
+    const resultMessage = document.getElementById('result')
+    if (score > 3) {
+        let resultText = '<span>Akamai! You got <p>' + score + '</p> out of <p>' + Questions.length + '</p></span>';
+        resultMessage.innerHTML = resultText;
+    } else if (score < 3) {
+        let resultText = '<span>Sorry, try again. You got<p>' + score + '</p> out of <p>' + Questions.length + '</p></span>';
+        resultMessage.innerHTML = resultText;
+    } else {
+        let resultText = '<span>debuggie' + score + '</p> out of <p>' + Questions.length + '</p></span>';
+        resultMessage.innerHTML = resultText;
+    }
+}
+
+
 
